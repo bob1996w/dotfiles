a90ae6c8a9ab61cfbfebe5ba723256d9b3beeba1
bob1996w <bob1996w@gmail.com>
2019-11-22 00:32:29 +0800

    Add fcitx IMEs

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # fcitx
-sudo pacman -S fcitx fcitx-gtk2 fcitx-gtk3 fcitx-qt5 fcitx-ui-light fcitx-table-other
+sudo pacman -S fcitx fcitx-gtk2 fcitx-gtk3 fcitx-qt5 fcitx-ui-light fcitx-table-other fcitx-mozc fcitx-chewing
 touch ~/.pam_environment
 echo "GTK_IM_MODULE=fcitx" >> ~/.pam_environment
 echo "QT_IM_MODULE=fcitx" >> ~/.pam_environment
